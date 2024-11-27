7fb6893f843f2c9ccf46b4624e783aa8186158cf
Kartik <kartikpoojari29@gmail.com>
2024-11-27 16:43:39 +0530

    database test using supabase

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,15 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@r@^bugu%o3^a-(b5rm_9nrayluw83_$rb=g6-@ozyq&gva(v!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool) #FOR PRODUCTION
+
 
 ALLOWED_HOSTS = [
     'mediplus-j6xih9ca8-priya1229s-projects.vercel.app',
     'mediplusbackend.vercel.app',
     'backendmedi.vercel.app',
+    '.vercel.app',
     'localhost',
     '127.0.0.1',
     '127.0.0.1:8000',
@@ -89,26 +92,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-import os
-import dj_database_url
-
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgres://postgres:root@localhost:5432/mediplus')
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=DATABASE_URL
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
+    }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
 
 
 # Password validation
